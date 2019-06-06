8185ccb9d5614a4509eb3c16ef05395c943ad7da
BanjoSledge <martin.kelly4000@gmail.com>
2019-06-06 10:16:03 +1000

    README update

--- README.md
+++ README.md
@@ -30,6 +30,10 @@ Currently doesn't contain all the data structures.
 #### Hashing:
 * CuckooHashMap: A HashMap using the Cuckoo Hash Collision Algorithm. Uses the following 4 hash algorithms: SHA1, FNV1a_128, Murmur3_128 & MD5 (in no set order)
 
+#### Algorithms:
+* DammAlgorithm: Can calculate a check digit using Damm's Algorithm. Uses the QuasiGroupGenerator to perform calculations.
+* QuasiGroupGenerator: Can create QuasiGroups of size n with an order of magnitude n. Form a Latin Square. Is generated Randomly.
+
 ### Apache Licence 2.0
 Copyright [2018] [Martin Kelly]
 
--- src/main/groovy/groovydatastructuresalgorithms/Testing.groovy
+++ src/main/groovy/groovydatastructuresalgorithms/Testing.groovy
@@ -1,9 +1,29 @@
 package groovydatastructuresalgorithms
 
+import groovy.xml.MarkupBuilder
+
 class Testing {
 
     static void main(String[] args) {
 
+        def writer = new StringWriter()
+        def xml = new MarkupBuilder(writer)
+        String carName = 'HSV Maloo'
+        xml.model() {
+            car(name: carName, make: 'Holden', year: 2006) {
+                country('Australia')
+                record(type: 'speed', 'Production Pickup Truck with speed of 271kph')
+            }
+        }
+        def records = new XmlParser().parseText(writer.toString())
+        println writer.toString()
+
+        CuckooHashMap<Integer, Integer> CHM = new CuckooHashMap<>();
+        CHM.put(0,1);
+        CHM.put(1,2);
+        CHM.put(2,3);
+        CHM.put(3,4);
+
+        println CHM.indexOfKey(2)
     }
 }
-
